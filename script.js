d55adf85cf36f3c0424d28112950a7a9f5b7f0b9
yvette rodriguez <yvette.rodriguez@csedge.org>
2019-02-16 09:53:09 -0800

    so afr

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ let playBTn = document.getElementBYId("play")
 playBtn.addEventListener("click",move);
 function move(){
 	playBtn.style.top = Math.random() *100 +1 + "%";
+	playBtn.style.left = Math.random() *100 +1 + "%";
 }
