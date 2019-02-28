7c86480579defe955002fa0b88ceeaa4f7927b48
yvette rodriguez <yvette.rodriguez@csedge.org>
2019-02-28 07:42:36 -0800

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,20 @@ function move(){
 	playBtn.style.top = Math.random() *100 +1 + "%";
 	playBtn.style.left = Math.random() *100 +1 + "%";
 }
+//challenge 1
+let removeText = document.getElementById("challenge-one");
+let body = document.getElementByTagName("body")[0];
+removeText.addEventListener("dblclick", ifClicked);
+function ifClicked(){
+	body.removeChild(removeText);
+}
+playBtn.addEventListener("mouseover", hover);
+function hover(){
+	playBtn.style.backgroundColor= "blue";
+}
+
+let title = document.getElementById("page-title")
+title.addEventListener("copy",copy);
+function copy(){
+	alert("STOP")
+}
