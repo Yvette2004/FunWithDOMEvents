ec96f8234b2fa736559c239960de285efe98b51c
yvette rodriguez <yvette.rodriguez@csedge.org>
2019-03-15 17:22:10 -0700

    so far

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 let playBtn = document.getElementById("play-button");
 
-playBtn.addEventListener("click",move);
+playBtn.addEventListener("click", move);
 
-function move(){
-	playBtn.style.top = Math.random() *100 +1 + "%";
-	playBtn.style.left = Math.random() *100 +1 + "%";
+function move() {
+	playBtn.style.top = Math.random()*100 + "%";
+	playBtn.style.left = Math.random()*100 + "%";
 }
-//challenge 1
+
+//Challenge 1
 let removeText = document.getElementById("challenge-one");
 let bodyElements = document.getElementsByTagName("body");
-let body = document.getElementByTagName("body");
 let body = bodyElements[0];
+
 
 removeText.addEventListener("dblclick", ifClicked);
 
-function ifClicked(){
+function ifClicked() {
 	body.removeChild(removeText);
 }
-playBtn.addEventListener("mouseover", hover);
 
-function hover(){
-	playBtn.style.backgroundColor = "blue";
+//Challenge 2 
+
+playBtn.addEventListener("mouseover",hover);
+
+function hover() {
+	playBtn.style.backgroundColor = "purple";
 }
 
-let title = document.getElementById("page-title")
+
+//Challenge 3
+let title= document.getElementById("page-title");
 title.addEventListener("copy",copy);
-function copy(){
-	alert("STOP");
+
+function copy() {
+ 	alert("STOP!");
+
 }
